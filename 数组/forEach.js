d61adfc15f86efec3a6108de61273b5f0e09c9cf
Yuriuh <642192014@qq.com>
2020-09-14 21:26:49 +0800

    Complete forEach

---
+++ 数组/forEach.js
@@ -0,0 +1,17 @@
+// var arr = [1, 2, 3, 4, 5]
+// arr.forEach((item, index, array) => {
+//   console.log('item', item)
+// })
+
+Array.prototype.myForEach = function (fn) {
+  fn = fn || (() => {})
+  for (let i = 0; i < this.length; i++) {
+    const item = this[i]
+    fn(item, i, this)
+  }
+}
+
+var arr = [1, 2, 3, 4, 5]
+arr.myForEach((item, index, array) => {
+  console.log(item, index, array)
+})
