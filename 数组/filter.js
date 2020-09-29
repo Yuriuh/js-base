b3f67889af36a72a38a510b74756906ab5a67117
Yuriuh <642192014@qq.com>
2020-09-29 22:36:17 +0800

    Complete myFilter

--- 数组/filter.js
+++ 数组/filter.js
@@ -1,5 +1,23 @@
+// var arr = [1, 2, 3, 4, 5]
+
+// var newArr = arr.filter(item => item > 2)
+
+// console.log('newArr', newArr)
+
+Array.prototype.myFilter = function (fn) {
+  fn = fn || (() => {})
+  const result = []
+  for (let i = 0; i < this.length; i++) {
+    const item = this[i]
+    if (fn(item, i, this)) {
+      result.push(item)
+    }
+  }
+  return result
+}
+
 var arr = [1, 2, 3, 4, 5]
 
-var newArr = arr.filter(item => item > 2)
+var newArr = arr.myFilter(item => item > 2)
 
 console.log('newArr', newArr)
