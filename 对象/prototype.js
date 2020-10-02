eecb469963739238c99718edb5b93a50be3b3257
Yuriuh <642192014@qq.com>
2020-10-02 21:51:55 +0800

    Add fn proto

--- 对象/prototype.js
+++ 对象/prototype.js
@@ -2,4 +2,10 @@ var obj = {}
 
 var proto = obj.__proto__
 
-console.log('proto', proto)
+console.log('obj proto', proto)
+
+var fn = () => {}
+
+var fnProto = fn.__proto__
+
+console.log('fn proto', fnProto)
