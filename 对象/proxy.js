37a0de1e06b8cfa5c9040600b6904fce2824ae21
Yuriuh <642192014@qq.com>
2020-10-26 23:38:01 +0800

    Add proxy demo

--- 对象/proxy.js
+++ 对象/proxy.js
@@ -16,3 +16,21 @@ Object.defineProperty(user, 'fullName', {
 console.log(user.fullName) // John Smith
 
 for (let key in user) console.log(key) // name, surname
+
+function User(name, birthday) {
+  this.name = name
+  this.birthday = birthday
+
+  // 年龄是根据当前日期和生日计算得出的
+  Object.defineProperty(this, 'age', {
+    get() {
+      let todayYear = new Date().getFullYear()
+      return todayYear - this.birthday.getFullYear()
+    },
+  })
+}
+
+let john = new User('John', new Date(1992, 6, 1))
+
+alert(john.birthday) // birthday 是可访问的
+alert(john.age) // ……age 也是可访问的
