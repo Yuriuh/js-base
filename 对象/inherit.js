a71dce9ae3138fcd351df546506c0dc91e83b6a5
Yuriuh <642192014@qq.com>
2020-10-27 23:47:31 +0800

    Update inherit.js

--- 对象/inherit.js
+++ 对象/inherit.js
@@ -42,3 +42,72 @@ var 奇克 = new 狮('奇克')
 
 console.log(奇克)
 console.log('奇克 吃', 奇克.叫)
+
+// 所谓的借用构造函数实现继承的模式
+// 垃圾
+
+// function 猫科(options) {
+//   this.颜色 = options.颜色
+// }
+
+// function 狮子(options) {
+//   猫科.call(this, options)
+//   this.名字 = options.名字
+// }
+
+// var 铠 = new 狮子({
+//   名字: '铠',
+//   颜色: '白色'
+// })
+
+// 如何给父类传递属性
+
+// function 猫科(options) {
+//   this.颜色 = options.颜色
+// }
+
+// 猫科.prototype.爬树 = function() {}
+// 猫科.prototype.叫 = function() {}
+
+// function 狮子(options) {
+//   猫科.call(this, options)
+//   this.名字 = options.名字
+// }
+
+// 狮子.prototype.__proto__ = 猫科.prototype
+
+继承(狮子, 猫科)
+
+// function 继承(继承者, 被继承者) {
+//   var F = function() {}
+//   F.prototype = 被继承者.prototype
+//   继承者.prototype = new F()
+//   继承者.prototype.constructor = 继承者
+// }
+
+// var 马利什 = new 狮子({
+//   名字: '马利什',
+//   颜色: '棕色',
+// })
+
+// es6 代码
+
+// class 猫科 {
+//   constructor(options) {
+//     this.颜色 = options.颜色
+//   }
+//   叫() {}
+//   爬树() {}
+// }
+
+// class 狮子 extends 猫科 {
+//   constructor(options) {
+//     super(options)
+//     this.名字 = options.名字
+//   }
+// }
+
+// var 马利什 = new 狮子({
+//   名字: '马利什',
+//   颜色: '棕色',
+// })
