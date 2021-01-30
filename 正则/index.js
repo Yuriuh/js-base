f5b68423b648438cb4f4871dacd34b97aa7d2bcd
Yuriuh <642192014@qq.com>
2021-01-30 23:41:02 +0800

    增加 search 方法

--- 正则/index.js
+++ 正则/index.js
@@ -1,4 +1,11 @@
 // 模式 + 修饰符
-var regexp = new RegExp('pattern', 'flags')
+// var regexp = new RegExp('pattern', 'flags')
 
 var regexp = /pattern/gim // 伴随修饰符 g、m 和 i（后面会讲到）
+
+// search 方法
+var str = 'I love JavaScript!'
+
+var regexp = /love/
+
+console.log('search string', str.search(regexp))
