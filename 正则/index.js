fcfb33ce299d7710de3732a0cb100a50f2423b29
Yuriuh <642192014@qq.com>
2021-03-19 23:55:42 +0800

    增加示例

--- 正则/index.js
+++ 正则/index.js
@@ -353,3 +353,11 @@ console.log(result.length) // 2
 // 其他信息：
 console.log(result.index) // 7（匹配位置）
 console.log(result.input) // I love JavaScript（源字符串）
+
+// 带 g
+var str = 'I love JavaScript'
+
+var result = str.match(/Java(Script)/g)
+
+console.log(result[0]) // JavaScript
+console.log(result.length) // 1
