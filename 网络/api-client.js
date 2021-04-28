6f481e310ba6fbdbb67b3f5b1e324dfeb221a205
Yuriuh <642192014@qq.com>
2021-04-28 23:55:11 +0800

    完成 Request get 方法

--- 网络/api-client.js
+++ 网络/api-client.js
@@ -1,5 +1,12 @@
-class Request {
-  get() {}
+export class Request {
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+    })
+  }
   post() {}
   put() {}
   delete() {}
