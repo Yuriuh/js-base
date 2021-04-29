3cf9a6546cf13d05741ea1f56468b4e61e9c1914
Yuriuh <642192014@qq.com>
2021-04-29 23:45:22 +0800

    封装 post 方法

--- 网络/api-client.js
+++ 网络/api-client.js
@@ -7,7 +7,20 @@ export class Request {
         .catch(err => reject(err))
     })
   }
-  post() {}
+  post() {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+    })
+  }
   put() {}
   delete() {}
 }
